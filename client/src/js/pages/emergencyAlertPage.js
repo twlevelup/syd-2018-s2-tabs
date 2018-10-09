45bd1c5acfbec29323a1fabc4b65bf62ea09bcf2
ken7y <kenwang2227@gmail.com>
2018-10-09 20:15:52 +1100

    Added cross to emergency page but need to fix gif issue

--- client/src/js/pages/emergencyAlertPage.js
+++ client/src/js/pages/emergencyAlertPage.js
@@ -1,6 +1,12 @@
 const BasePage = require('watch-framework').BasePage;
+const countdowngif = require('../../images/logo.png');
+const cross = require('../../images/cross.png');
 
 class EmergencyAlertPage extends BasePage {
+  pageWillLoad() {
+    this.countdowngif = countdowngif;
+    this.cross = cross;
+  }
   template = require('../../templates/emergencyAlertPage.hbs');
 }
 
