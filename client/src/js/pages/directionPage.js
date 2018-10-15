c118fa6b0cb38df3ed24dc3437836be53b8753b0
Anna Tran <anhkaehne@gmail.com>
2018-10-15 22:41:11 +1100

    changed navigation for home button from left to bottom

--- client/src/js/pages/directionPage.js
+++ client/src/js/pages/directionPage.js
@@ -1,7 +1,7 @@
 const BasePage = require('watch-framework').BasePage;
 class DirectionPage extends BasePage {
   template = require('../../templates/directionPage.hbs');
-  leftButtonEvent() {
+  bottomButtonEvent() {
     this.navigate('/');
   }
 }
