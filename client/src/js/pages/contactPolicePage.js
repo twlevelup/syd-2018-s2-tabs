cfdd03a46b16459d4d8e560d83276c5d1126e441
lzhou <lillianzhou87@gmail.com>
2018-10-16 23:42:52 +1100

    [Lillian] Added missing arrow icons

--- client/src/js/pages/contactPolicePage.js
+++ client/src/js/pages/contactPolicePage.js
@@ -8,7 +8,7 @@ class contactPolicePage extends BasePage {
   template = require('../../templates/contactPolicePage.hbs');
 
   pageWillLoad() {
-    this.icon = police;
+    this.police = police;
     this.cross = cross;
     this.left = left;
     this.right = right;
