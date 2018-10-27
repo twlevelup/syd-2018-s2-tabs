d9c80e67c1470f79439fee803368fafb45b9b8b7
ellijayne <ellijayne.m.willard@gmail.com>
2018-10-27 13:39:14 +1100

    changed styling on walking game

--- client/src/js/pages/emergencyAlertPage.js
+++ client/src/js/pages/emergencyAlertPage.js
@@ -18,6 +18,7 @@ class EmergencyAlertPage extends TabsBasePage {
     this.countdowngif = countdowngif;
     this.cross = cross;
   }
+
   template = require('../../templates/emergencyAlertPage.hbs');
 
   bottomButtonEvent() {
@@ -26,7 +27,6 @@ class EmergencyAlertPage extends TabsBasePage {
 
     this.navigate('/');
   }
-
 }
 
 module.exports = EmergencyAlertPage;
