84e55538f9eb373d9221e4e351bee7c30bec0c5f
ken7y <kenwang2227@gmail.com>
2018-10-27 12:44:43 +1100

    [Ken] attempt at fixing issue

--- client/src/js/routes.js
+++ client/src/js/routes.js
@@ -55,7 +55,7 @@ module.exports = {
   'successmed': SuccessMedPage,
   'walkingActivity': WalkingActivityPage,
   'failmed': FailMedPage,
-  'takemed': MedicinePage
+  'takemed': MedicinePage,
   'doneWalking': DoneWalkingPage
 
 };
